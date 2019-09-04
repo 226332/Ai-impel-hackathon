dc84f929bb3923b1fb2a516a8cd117baa70da2e3
azema <arkadiusz.zema@intel.com>
2019-09-04 10:52:04 +0200

    Merge components up

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import sys
+from solver import Solver
 
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%H:%M:%S')
 
@@ -9,13 +10,16 @@ def create_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', default="train", choices=["train", "test"], required=True)
     parser.add_argument('--input_size', default=32)
-    parser.add_argument('--batch-size', default=64)
+    parser.add_argument('--batch_size', default=64)
     parser.add_argument('--gpu', default=0, choices=[0, 1, 2, 3])
+    parser.add_argument('--train_iter', default=100)
     return parser
 
 
 def main(config):
     logging.info(f"Program started with given config {vars(config)}")
+    loader1, loader2 = ("loader1", "loader2")
+    solver = Solver(config, loader1, loader2)
     # TODO trainer/model/loader
     return 1
 
