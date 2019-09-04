9af01386ab02ead0e203bc6f87e1123c51a50e01
azema <arkadiusz.zema@intel.com>
2019-09-04 11:53:23 +0200

    add arg

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def create_parser():
     parser.add_argument('--mode', default="train", choices=["train", "test"], required=True)
     parser.add_argument('--input_size', default=32)
     parser.add_argument('--batch_size', default=64)
+    parser.add_argument('--conv_dim', default=32)
     parser.add_argument('--gpu', default=0, choices=[0, 1, 2, 3])
     parser.add_argument('--train_iter', default=100)
     parser.add_argument('--dataset_name', required=True, type=str)
