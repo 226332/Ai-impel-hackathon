422dcd811d080357ac9b5b88348f814d51d6b917
Markiewicz <emil.markiewicz@intel.com>
2019-09-05 19:06:31 +0200

    hejka

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 CORS(app)
 PATH_TO_PYTHON_INTERPRETER = 'python'
 PATH_TO_DUPA = f'/root/hackathon/project/models/g12.pkl'
+PATH_TO_DUPA2 = f'/root/hackathon/project/models/g21.pkl'
 PATH_TO_SCRIPT = '/root/hackathon/project/test.py'
 @app.route('/')
 def hello_world():
@@ -14,13 +15,13 @@ def hello_world():
 @app.route('/xd', methods=['GET', 'POST'])
 def tranform_request():
     mode = request.args.get("type")
-
+    model_path = PATH_TO_DUPA if mode == 'green' else PATH_TO_DUPA2
     file = request.files['file']
     path_to_image = os.path.join(os.path.dirname(os.path.realpath(__file__)), f"focia{datetime.datetime.now().timestamp()}.jpg")
     file.save(path_to_image)
     ###
     d=os.path.join(os.path.join(os.path.dirname(os.path.realpath(__file__)), "build"), "img.jpg")
-    x=f'{PATH_TO_PYTHON_INTERPRETER} {PATH_TO_SCRIPT} --img_path "{path_to_image}" --gen_path {PATH_TO_DUPA} --out {d}'
+    x=f'{PATH_TO_PYTHON_INTERPRETER} {PATH_TO_SCRIPT} --img_path "{path_to_image}" --gen_path {model_path} --out {d}'
     print(x)
     os.system(x)
 
